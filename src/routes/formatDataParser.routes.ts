0de4ca91aa7eae4f9bef1e92a8aa8fefb2adc9e0
Amol Dawane <amol.dawane.it@ghrcem.raisoni.net>
2025-06-09 22:20:38 +0530

    new apis created

--- src/routes/formatDataParser.routes.ts
+++ src/routes/formatDataParser.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { formatDataParser1, formatDataParser2 } from '../controllers/formatDataParser.controller';
+import { formatDataParser1, formatDataParser2,formatDataParser3 } from '../controllers/formatDataParser.controller';
 
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/parser1', formatDataParser1);
 router.get('/parser2', formatDataParser2);
+router.get('/parser3',formatDataParser3)
 
 
 export default router;
--- src/utils/TransformerFactory.ts
+++ src/utils/TransformerFactory.ts
@@ -1,8 +1,9 @@
 import { schema1 } from "./schema1";
 import { schema2 } from "./schema2";
 import { JsonTransformer } from "./parser";
+import { schema3 } from "./schema3";
 
-export type PatientType = "patient" | "patient2";
+export type PatientType = "patient" | "patient2" | "patient3";
 
 export class TransformerFactory {
   static create(type: PatientType): JsonTransformer {
@@ -11,6 +12,8 @@ export class TransformerFactory {
          return new JsonTransformer(schema1);
       case "patient2":
         return new JsonTransformer(schema2);
+      case "patient3":
+        return new JsonTransformer(schema3);
       default:
         throw new Error("Unknown transformer type");
     }
