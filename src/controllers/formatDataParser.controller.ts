8612e926c8e3b450c9485dc25dc61f7dc219df87
Amol Dawane <amol.dawane.it@ghrcem.raisoni.net>
2025-07-22 18:56:34 +0530

    token update

--- src/controllers/formatDataParser.controller.ts
+++ src/controllers/formatDataParser.controller.ts
@@ -8,8 +8,7 @@ import createParser from "../parserFactory";
 export const formatDataParser1 = async (req: Request, res: Response) => {
   try {
     const url: any = process.env.URL;
-    const token =
-      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFUWkNNemd4TnpKRk5UZzFSakJDT0RrMlFVTkZOREV3TlRCQlJFRXpNelV4UWpVeFF6bEVRUSJ9.eyJpc3MiOiJodHRwczovL29uZWRlcmZ1bC1kZW50YWwuYXV0aDAuY29tLyIsInN1YiI6InJuc0g4Qk52bUQzMjl4UTU1VmtTRnFheU5qUWlOSVFiQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3Byb2R1Y3Rpb24ub25lZGVyZnVsLmNvIiwiaWF0IjoxNzUzMTgxOTA1LCJleHAiOjE3NTMyMTc5MDUsInNjb3BlIjoiZmVhdHVyZTplbGlnaWJpbGl0eSBmZWF0dXJlOmVuaGFuY2VkLWVsaWdpYmlsaXR5IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoicm5zSDhCTnZtRDMyOXhRNTVWa1NGcWF5TmpRaU5JUWIiLCJwZXJtaXNzaW9ucyI6WyJmZWF0dXJlOmVsaWdpYmlsaXR5IiwiZmVhdHVyZTplbmhhbmNlZC1lbGlnaWJpbGl0eSJdfQ.vgZ4gMV6fmY1FXLtAGbDL9EE9yDB6DubrtYQBGU1Zf8OXInUZY4DvD0gA8-dC58viC-NzK6Ic9x68VOpgelH97ZobHnxsEge_SvVWQInSdZVl5HlOaN70vzerfSCZqrTIks4cMjYG64lm0VSUnOux9Vdu6s_qoZ5lApUyPc1knyyOsusHFF0EEM6Ia-Va9DRArfZ62H_uLYvWRL9qF0VbWh2dqs_A6bilWysC0BVMcPJ2xmN9FCQgkf1bz5l2Z_ZPhXrFTkWzpk6VdPbiDPspFRSLSiK-lyfjN-UGnyVRi9HxBGpB9rVaCSxk9JBJ2yJ9vuNoSKumLos1xyDefpK1A";
+    const token = "" // paste your token here 
     const { payload } = req.body;
     const identifier = payload.payer.id;
     const onederfulPayerId =
